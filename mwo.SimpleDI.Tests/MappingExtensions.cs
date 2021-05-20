ebc1f444ef001eeadf4f3f3ede5ad6c407ac0f6a
Marius Wodtke <marius.wodtke@gmail.com>
2021-05-20 17:16:48 +0200

    Working Tests

---
+++ mwo.SimpleDI.Tests/MappingExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AutoMapper;
+
+namespace mwo.SimpleDI.Tests
+{
+    public static class MappingExtensions
+    {
+        private const string ContainerName = "Dependency";
+        public static void SetDependencyContainer(this IMappingOperationOptions opt, IDependencyContainer container)
+        {
+            opt.Items[ContainerName] = container;
+        }
+
+        public static IDependencyContainer GetDependencyContainer(this ResolutionContext opt)
+        {
+            var container = opt.Items[ContainerName] as DependencyContainer;
+            if (container == null)
+                throw new InvalidOperationException("Mapper requests DependencyContainer but none is specified!");
+            return container;
+        }
+        public static void MapWithDependency<TDependency, TSource, TDestination, TMember, TResult>(this IMemberConfigurationExpression<TSource, TDestination, TMember> config, Func<TSource, TDependency, TResult> func, Func<IDependencyContainer, TDependency> resolver)
+        {
+            config.MapFrom((s, d, m, c) => func(s, resolver.Invoke(c.GetDependencyContainer())));
+        }
+
+        public static TDestination Map<TSource, TDestination>(this IMapper mapper, TSource source, IDependencyContainer container)
+        {
+            return mapper.Map<TDestination>(source, _ => _.SetDependencyContainer(DependencyContainerManager.Default));
+        }
+    }
+}
--- mwo.SimpleDI.Tests/UnitTest1.cs
+++ mwo.SimpleDI.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
 
 namespace mwo.SimpleDI.Tests
 {
@@ -7,6 +8,14 @@ namespace mwo.SimpleDI.Tests
     public class UnitTest1
     {
         private IMapper _mapper;
+        private Address address = new Address()
+        {
+            City = "Vienna",
+            pub_Country = "Austria",
+            Id = 1,
+            Street = "Schottenring",
+            MyProp = "Test"
+        };
 
         [TestMethod]
         public void TestMethod1()
@@ -14,22 +23,29 @@ namespace mwo.SimpleDI.Tests
             Initialize();
             var res = Map();
 
-            Assert.IsTrue(true);
+            Assert.IsNotNull(res);
         }
 
         public AddressDTO Map()
         {
-            return _mapper.Map<Address, AddressDTO>(new Address() { City = "istanbul", Country = "turkey", Id = 1, Street = "istiklal cad." });
+            return _mapper.Map<Address, AddressDTO>(address, DependencyContainerManager.Default);
         }
 
         public void Initialize()
         {
+            DependencyContainerManager.Default.RegisterStrong<INamingConvention>(_ => new LowerNamingConvention());
+
             var config = new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<Address, Address>();
-                cfg.CreateMap<Address, AddressDTO>();
+                cfg.RecognizePrefixes("pub_");
+                cfg.RecognizeDestinationPrefixes("dat_");
+                cfg.CreateMap<Address, AddressDTO>()
+                    //.ForMember(_ => _.unmapped, _ => _.Ignore())
+                    //.ForMember(_ => _.unmapped, _ => _.MapWithDependency((_, dep) => dep.SeparatorCharacter, _ => _.Resolve<INamingConvention>()))
+                    .ForMember(_ => _.line1, _ => _.MapFrom(_ => $"{_.Street} {_.Number}"));
             });
-            config.AssertConfigurationIsValid();
+            //config.AssertConfigurationIsValid();
+            //var executionPlan = config.BuildExecutionPlan(typeof(Address), typeof(AddressDTO));
             _mapper = config.CreateMapper();
         }
 
@@ -37,15 +53,28 @@ namespace mwo.SimpleDI.Tests
         {
             public int Id { get; set; }
             public string Street { get; set; }
+            public int Number { get; set; }
             public string City { get; set; }
-            public string Country { get; set; }
+            public string pub_Country { get; set; }
+            public string MyProp { get; set; }
         }
 
         public class AddressDTO
         {
-            public int Id { get; set; }
-            public string City { get; set; }
-            public string Country { get; set; }
+            public int id { get; set; }
+            public string city { get; set; }
+            public string country { get; set; }
+            public string dat_myprop { get; set; }
+            public string unmapped { get; set; }
+            public string line1 { get; set; }
+        }
+
+        public class LowerNamingConvention : INamingConvention
+        {
+            private static readonly Regex LowerCase = new Regex(@"(\p{Lu}+(?=$|\p{Lu}[\p{Ll}0-9])|\p{Lu}?[\p{Ll}0-9]+)");
+            public Regex SplittingExpression { get; } = LowerCase;
+            public string SeparatorCharacter => "Dependency";//string.Empty;
+            public string ReplaceValue(Match match) => match.Value[0].ToString().ToUpper() + match.Value.Substring(1);
         }
     }
 }
